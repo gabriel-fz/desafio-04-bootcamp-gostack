f860d14c61adc7024ec20b86c420a749624946a9
gabriel-fz <gabrielfzancanela@gmail.com>
2019-10-02 17:30:38 -0300

    adicionando os textos dos posts

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -20,28 +20,74 @@ class PostList extends Component {
               avatar:
                 "https://www.verasport.pl/pub/skin/default-skin/img/avatar.png"
             },
-            content: "Conteúdo do comentário"
+            content:
+              "A Rocketseat está sempre em busca de novos membros para o time, e geralmente ficamos de olho em quem se destaca no Bootcamp, inclusive 80% do nosso time de devs é composto por alunos do Bootcamp. Além disso, se você tem vontade de ensinar gravando vídeos e criando posts, pode me chamar no Discord! (Sério, me chamem mesmo, esse comentário é real)"
           }
         ]
       },
       {
         id: 2,
         author: {
-          name: "Gabriel Fiorese",
+          name: "Gabriel Lisboa",
           avatar:
             "https://www.verasport.pl/pub/skin/default-skin/img/avatar.png"
         },
         date: "04 Jun 2019",
-        content: "Pessoal, alguém sabe se a Rocketseat está contratando?",
+        content:
+          "Fala galera, beleza? Estou fazendo o Bootcamp GoStack e está sendo muito massa! Alguém mais aí fazendo? Comenta aí na publicação para trocarmos uma idéia",
         comments: [
           {
             id: 1,
             author: {
-              name: "Diego Fernandes",
+              name: "Clara Lisboa",
               avatar:
                 "https://www.verasport.pl/pub/skin/default-skin/img/avatar.png"
             },
-            content: "Conteúdo do comentário"
+            content:
+              "Também estou fazendo o Bootcamp e estou adorando! Estou no terceiro módulo sobre Node e já tenho minha API dos desafios construída!"
+          },
+          {
+            id: 2,
+            author: {
+              name: "Cézar Toledo",
+              avatar:
+                "https://www.verasport.pl/pub/skin/default-skin/img/avatar.png"
+            },
+            content:
+              "Que maaaaaassa! Estou pensando em me inscrever na próxima turma pra ver qual é desse Bootcamp GoStack, dizem que os devs saem de lá com super poderes"
+          }
+        ]
+      },
+      {
+        id: 3,
+        author: {
+          name: "Gabriel Lisboa",
+          avatar:
+            "https://www.verasport.pl/pub/skin/default-skin/img/avatar.png"
+        },
+        date: "04 Jun 2019",
+        content:
+          "Fala galera, beleza? Estou fazendo o Bootcamp GoStack e está sendo muito massa! Alguém mais aí fazendo? Comenta aí na publicação para trocarmos uma idéia",
+        comments: [
+          {
+            id: 1,
+            author: {
+              name: "Clara Lisboa",
+              avatar:
+                "https://www.verasport.pl/pub/skin/default-skin/img/avatar.png"
+            },
+            content:
+              "Também estou fazendo o Bootcamp e estou adorando! Estou no terceiro módulo sobre Node e já tenho minha API dos desafios construída!"
+          },
+          {
+            id: 2,
+            author: {
+              name: "Cézar Toledo",
+              avatar:
+                "https://www.verasport.pl/pub/skin/default-skin/img/avatar.png"
+            },
+            content:
+              "Que maaaaaassa! Estou pensando em me inscrever na próxima turma pra ver qual é desse Bootcamp GoStack, dizem que os devs saem de lá com super poderes"
           }
         ]
       }
