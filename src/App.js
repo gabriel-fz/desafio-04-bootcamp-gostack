348c0c441ff7d6f82d1256c2945a28a2614757d6
gabriel-fz <gabrielfzancanela@gmail.com>
2019-10-02 15:04:45 -0300

    importando imagens

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import "./App.css";
 
+import profile from "./assets/foto-uenf.png";
+
 function App() {
-  return <h1>Hello Fiorese</h1>;
+  return <img width="200" src={profile} />;
 }
 
 export default App;
